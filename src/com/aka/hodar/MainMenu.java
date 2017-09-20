05bcd69faa58cd63569ab3f6df6cf4229324d671
Unknown <rtomilobil@gmail.com>
2017-09-20 08:41:37 +0200

    add simple menu

--- src/com/aka/hodar/MainMenu.java
+++ src/com/aka/hodar/MainMenu.java
@@ -1,30 +1,84 @@
 package com.aka.hodar;
 
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
-
-import java.util.Stack;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class MainMenu extends Pane {
+
+    VBox vBox;
+    boolean isClicked;
+    ClassTypes selectedClass;
 
     public MainMenu(){
         setTableBackground(new Image("bgv2.jpg"));
 
-        StackPane imageStack = new StackPane();
-        imageStack.setLayoutX(100);imageStack.setLayoutY(100);
-        ImageView imgV = new ImageView(new Image("images/classes/warrior/warrior.png"));
-        imageStack.getChildren().add(imgV);
-
-        StackPane nameStack = new StackPane();
-        nameStack.setLayoutX(500);nameStack.setLayoutY(500);
+        StackPane namePane = new StackPane();
+        namePane.setLayoutX(Globals.SCREEN_WIDTH/2-100);namePane.setLayoutY(Globals.SCREEN_HEIGHT- Globals.SCREEN_HEIGHT/4);
         TextField nameField = new TextField();
-        nameStack.getChildren().add(nameField);
+        nameField.setText("Name of your character");
+        namePane.getChildren().add(nameField);
+
+        getChildren().addAll(namePane);
+
+        double xCoordinates = 100;
+        double yCoordinates = Globals.SCREEN_HEIGHT/2-Globals.warriorImage.getHeight()/2;
+        for (ClassTypes classType: ClassTypes.values()) {
+            ImageView charImage = new ImageView();
+            StackPane imageStack = new StackPane();
+
+            charImage.setScaleX(0.75);charImage.setScaleY(0.75);
+            imageStack.setLayoutX(xCoordinates);imageStack.setLayoutY(yCoordinates);
+
+            charImage.setImage(classType.getImage());
+            charImage.setOnMouseClicked(event -> {
+                if (isClicked) getChildren().remove(vBox);
+                setVbox(classType);
+                selectedClass = classType;
+                getChildren().add(vBox);
+                isClicked = !isClicked;
+            });
+            xCoordinates += Globals.warriorImage.getWidth();
+            imageStack.getChildren().addAll(charImage);
+            getChildren().addAll(imageStack);
+        }
+
+        Button btn = new Button();
+        btn.setText("Play");
+        btn.setOnMouseClicked(event -> {
+            System.out.println(selectedClass);
+            if (selectedClass != null){
+                System.out.println(getChildren());
+                Game game = new Game(selectedClass);
+                Globals.primaryStage.setScene(new Scene(game, Globals.SCREEN_WIDTH, Globals.SCREEN_HEIGHT));
+                Globals.primaryStage.show();
+            }
+
+        });
+        btn.setLayoutX(Globals.SCREEN_WIDTH/2-nameField.getWidth());btn.setLayoutY(Globals.SCREEN_HEIGHT- Globals.SCREEN_HEIGHT/4+35);
+        getChildren().add(btn);
 
 
+    }
 
-        getChildren().addAll(nameStack, imageStack);
+    private VBox setVbox(ClassTypes classType){
+        String infoBuilder = String.format("Class: %s\n\n%s\n\nAgility: %s\nIntellect: %s\nStrength: %s\nHealth: %s",
+                classType.getClassName(), classType.getStory(), classType.getAgility(),
+                classType.getIntellect(), classType.getStrength(), classType.getHealth());
+        vBox = new VBox();
+        Text text = new Text(infoBuilder);
+        text.getFont();
+        vBox.getChildren().add(text);
+        vBox.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        vBox.setLayoutX(3*Globals.SCREEN_WIDTH/5+75);vBox.setLayoutY(Globals.SCREEN_HEIGHT/3);
+        return vBox;
     }
 
     void setTableBackground(Image tableBackground){
