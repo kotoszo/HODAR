1d3fdc8138cc65868e06e4349aaa32878f67958b
Unknown <rtomilobil@gmail.com>
2017-09-21 15:26:34 +0200

    update classtypes enum

--- src/com/aka/hodar/ClassTypes.java
+++ src/com/aka/hodar/ClassTypes.java
@@ -2,14 +2,16 @@ package com.aka.hodar;
 
 import javafx.scene.image.Image;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum ClassTypes {
 
-    WARRIOR("Warrior", 3, 1, 5, 125, "Warriors are good in close-combat.\n9/10 warriors recommend it!", new Image("images/classes/warrior/warrior.png"), Skills.WARRIOR),
-    MAGE("Mage", 3, 5, 1, 150, "Mages are well known magic users.\nThey can deal big amount of damage.", new Image("images/classes/mage/mage.png"), Skills.MAGE),
-    PIRATE("Pirate", 4, 1, 4, 200, null, new Image("images/classes/pirate/pirate.png"), null);
+    WARRIOR("WARRIOR", 3, 1, 5, 250, "Warriors are good in close-combat.\n9/10 warriors recommend it!", Images.WARRIOR.getImage(), Skills.WARRIOR),
+    MAGE("MAGE", 3, 5, 1, 150, "Mages are well known magic users.\nThey can deal big amount of damage.", Images.MAGE.getImage(), Skills.MAGE),
+    PIRATE("Pirate", 4, 1, 4, 200, null, Images.PIRATE.getImage(), null),
+    DRUID("Druid", 4, 3, 2, 225, "Druids are friends of mother nature.\nasd", Images.DRUID.getImage(), null),
+    PEASENT("Peasent", 4, 1, 4, 200, "Peasent are well known for nothing.\n But, they can achive something.", Images.PEASENT.getImage(), null),
+    WIZARD("Wizard", 1, 7, 1, 175, "Wizards are rare, just like a bubblegum\n... in the middle ages.", Images.WIZARD.getImage(), null),
+    THIEF("Thief", 6, 2, 1, 0,"Thieves are weaks in mid-ranged combat.\n But deadly in close-combat.", Images.THIEF.getImage(), null),
+    MARTIAL("Martial Artist", 4, 2, 3, 0, "Martial artists are using first weapons\n..or barehanded.", Images.MARTIAL.getImage(), null);
 
     private int health;
     private Image image;
